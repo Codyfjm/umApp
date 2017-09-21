43e4d2ed30be2416f07f26f7fb0c4001daef0301
cody <629523179@qq.com>
2017-09-21 09:35:42 +0800

    1.9.21

---
+++ UmAppDemo/app/src/main/java/com/umeng/example/MainActivity.java
@@ -0,0 +1,26 @@
+package com.umeng.example;
+
+import com.umeng.example.analytics.AnalyticsHome;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        int id = v.getId();
+        Intent in = null;
+        if (id == R.id.normal) {
+            in = new Intent(this, AnalyticsHome.class);
+        }
+        startActivity(in);
+    }
+
+}
